1d53bcb9fd4ff66bc3cb180e903922e0033ce3d2
jaromo0925 <jaromo25@gmail.com>
2025-03-24 23:29:41 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const os = require('os');
+//const os = require('os');
 const path = require('path');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
-
+//const PORT = 3000;
+const PORT = process.env.PORT || 3000;  // Ajustado para Vercel
 
 // Habilita CORS para todos los orígenes
 app.use(cors());
@@ -20,28 +20,8 @@ app.get('/', (req, res) => {
 });
 
 
-// Configura la ruta principal
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/index.html'));
-});
-
-// Función para obtener la IP local
-function getLocalIP() {
-    const interfaces = os.networkInterfaces();
-    for (const interfaceName in interfaces) {
-        for (const net of interfaces[interfaceName]) {
-            if (net.family === 'IPv4' && !net.internal) {
-                return net.address;
-            }
-        }
-    }
-    return '127.0.0.1'; // Fallback a localhost si no encuentra otra IP
-}
 
 // Inicia el servidor
-const localIP = getLocalIP();
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Servidor corriendo en:`);
-    console.log(`- Local:   http://localhost:${PORT}`);
-    console.log(`- Red:    http://${localIP}:${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
 });
