4ee68274836c7c5573e949eb4f82f427fe1459f8
jaromo0925 <jaromo25@gmail.com>
2025-03-26 10:47:23 -0500

    Update lineas.js

--- public/js/lineas.js
+++ public/js/lineas.js
@@ -121,6 +121,12 @@ function actualizarGraficosProductos(datos) {
             axisLabel: { rotate: 45 }
         },
         yAxis: { type: 'value' },
+        grid: { 
+            top: 100,   // Deja más espacio arriba
+            bottom: 50, 
+            left: 50, 
+            right: 50 
+    },
         legend: { data: tiposDeProductos,
             top: 10,
             left: "center"},
