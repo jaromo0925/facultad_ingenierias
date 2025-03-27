285e2320e1a4192ea4ff5090763a47f87081dab7
jaromo0925 <jaromo25@gmail.com>
2025-03-27 16:20:03 -0500

    Update tablappal.js

--- public/js/tablappal.js
+++ public/js/tablappal.js
@@ -40,7 +40,7 @@ $(document).ready(function () {
     const radarOptionsProyectos = {
         title: { text: 'Contribución por Docentes a Proeyctos Estratégicos',
         textStyle: {
-            fontSize: 30,       // Tamaño de la fuente
+           // fontSize: 30,       // Tamaño de la fuente
             fontWeight: 'bold', // Negrita
             color: '#333'       // Color del texto
         },
