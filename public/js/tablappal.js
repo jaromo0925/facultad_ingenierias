73277c1dee20adaaf7ee347d05afc3b298871812
jaromo0925 <jaromo25@gmail.com>
2025-03-28 01:34:19 +0000

    Update tablappal.js

--- public/js/tablappal.js
+++ public/js/tablappal.js
@@ -54,7 +54,7 @@ $(document).ready(function () {
         text: 'Contribución Docente - Microproyectos Estratégicos',
         left: 'center', 
         textStyle: {
-            fontSize: 18 // Ajusta el tamaño según lo necesites
+            fontSize: 22 // Ajusta el tamaño según lo necesites
         }
     },
     tooltip: {},
